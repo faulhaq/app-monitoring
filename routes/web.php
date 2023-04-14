912fd2e4c0b09c91eddef0357a713abb44adeaad
Syifa' Ul Haq M <syifaulhaq80@gmail.com>
2023-04-14 20:58:13 +0700

    Add template bootstrap 5
    Signed-off-by: Syifa' Ul Haq M <syifaulhaq80@gmail.com>

--- routes/web.php
+++ routes/web.php
@@ -14,10 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
 
-
-Route::get('/tes', function(){
-    echo 'tes';
-});
