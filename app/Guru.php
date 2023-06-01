f949139cf38a9d4752de398e224c43b54395064c
Syifa' Ul Haq M <syifaulhaq80@gmail.com>
2023-06-01 11:28:20 +0700

    update model guru.php
    Signed-off-by: Syifa' Ul Haq M <syifaulhaq80@gmail.com>

--- app/Guru.php
+++ app/Guru.php
@@ -9,12 +9,7 @@ class Guru extends Model
 {
     use SoftDeletes;
 
-    protected $fillable = ['id_card', 'nip', 'nama_guru', 'mapel_id', 'kode', 'jk', 'telp', 'tmp_lahir', 'tgl_lahir', 'foto'];
-
-    public function mapel()
-    {
-        return $this->belongsTo('App\Mapel')->withDefault();
-    }
+    protected $fillable = ['id_card', 'nip', 'nama_guru', 'kode', 'jk', 'telp', 'tmp_lahir', 'tgl_lahir', 'goldar', 'foto'];
 
     public function dsk($id)
     {
