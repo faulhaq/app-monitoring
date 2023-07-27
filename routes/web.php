4999dac3d4f0296fb28af8414263833d57a746b3
Syifa' Ul Haq M <syifaulhaq80@gmail.com>
2023-07-27 16:43:39 +0700

    restore route user controller
    Signed-off-by: Syifa' Ul Haq M <syifaulhaq80@gmail.com>

--- routes/web.php
+++ routes/web.php
@@ -12,11 +12,11 @@
 */
 
 Auth::routes();
-// Route::get('/login/cek_email/json', 'UserController@cek_email');
-// Route::get('/login/cek_password/json', 'UserController@cek_password');
-// Route::post('/cek-email', 'UserController@email')->name('cek-email')->middleware('guest');
-// Route::get('/reset/password/{id}', 'UserController@password')->name('reset.password')->middleware('guest');
-// Route::patch('/reset/password/update/{id}', 'UserController@update_password')->name('reset.password.update')->middleware('guest');
+Route::get('/login/cek_email/json', 'UserController@cek_email');
+Route::get('/login/cek_password/json', 'UserController@cek_password');
+Route::post('/cek-email', 'UserController@email')->name('cek-email')->middleware('guest');
+Route::get('/reset/password/{id}', 'UserController@password')->name('reset.password')->middleware('guest');
+Route::patch('/reset/password/update/{id}', 'UserController@update_password')->name('reset.password.update')->middleware('guest');
 
 Route::middleware(['auth'])->group(function () {
     Route::get('/', 'HomeController@index')->name('home');
