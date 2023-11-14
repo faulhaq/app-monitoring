b09a057e8cb10243d67f8835df72d2221c90b666
Syifa' Ul Haq M <syifaulhaq80@gmail.com>
2023-11-14 13:38:47 +0700

    fix detail about me pada user guru
    Signed-off-by: Syifa' Ul Haq M <syifaulhaq80@gmail.com>

--- resources/views/user/pengaturan.blade.php
+++ resources/views/user/pengaturan.blade.php
@@ -4,6 +4,21 @@
   <li class="breadcrumb-item active">User Profile</li>
 @endsection
 @section('content')
+<?php
+$user = Auth::user();
+if ($user->role === "guru") {
+    $guru = $user->guru();
+    $foto = (!empty($guru->foto)) ? "uploads/guru/".$guru->foto : NULL;
+} else {
+    $orang_tua = $user->orang_tua();
+}
+
+if ($foto) {
+    $data_footer = '<a href="'.route('pengaturan.edit-foto').'" id="linkFotoGuru" class="btn btn-link btn-block btn-light"><i class="nav-icon fas fa-file-upload"></i> &nbsp; Ubah Foto</a>';
+}
+
+$name = $user->name();
+?>
 <div class="col-12">
     <div class="row">
         <div class="col-5">
@@ -11,39 +26,17 @@
             <div class="card card-primary card-outline">
                 <div class="card-body box-profile">
                     <div class="text-center">
-                    @if (Auth::user()->role == 'guru')
-                        @php $guru = Auth::user()->guru(); @endphp
-                        <a href="{{ asset('uploads/guru/'.$guru->foto) }}" data-toggle="lightbox" data-title="Foto {{ Auth::user()->name() }}" data-gallery="gallery" data-footer='<a href="{{ route('pengaturan.edit-foto') }}" id="linkFotoGuru" class="btn btn-link btn-block btn-light"><i class="nav-icon fas fa-file-upload"></i> &nbsp; Ubah Foto</a>'>
-                            <img src="{{ asset('uploads/guru/'.$guru->foto) }}" width="130px" class="profile-user-img img-fluid img-circle" alt="User profile picture">
-                        </a>
-                    @elseif (Auth::user()->role == 'Siswa')
-                        <a href="{{ asset(Auth::user()->siswa(Auth::user()->no_induk)->foto) }}" data-toggle="lightbox" data-title="Foto {{ Auth::user()->name() }}" data-gallery="gallery" data-footer='<a href="{{ route('pengaturan.edit-foto') }}" id="linkFotoGuru" class="btn btn-link btn-block btn-light"><i class="nav-icon fas fa-file-upload"></i> &nbsp; Ubah Foto</a>'>
-                            <img src="{{ asset(Auth::user()->siswa(Auth::user()->no_induk)->foto) }}" width="130px" class="profile-user-img img-fluid img-circle" alt="User profile picture">
+                    @if ($foto)
+                        <a href="{{ $foto }}" data-toggle="lightbox" data-title="Foto {{ $name }}" data-gallery="gallery" data-footer="{{ $data_footer }}">
+                            <img src="{{ $foto }}" width="130px" class="profile-user-img img-fluid img-circle" alt="User profile picture">
                         </a>
                     @else
                         <img class="profile-user-img img-fluid img-circle" src="{{ asset('img/male.jpg') }}" alt="User profile picture">
                     @endif
                     </div>
-                    <h3 class="profile-username text-center">{{ Auth::user()->name() }}</h3>
-                    <p class="text-muted text-center"> Role : {{ Auth::user()->role() }}</p>
-                    @if (Auth::user()->role == 'guru')
-                        <ul class="list-group list-group-unbordered mb-3">
-                            <li class="list-group-item">
-                                <b>NIP</b> <a class="float-right">{{ Auth::user()->guru(Auth::user()->id_card)->nip }}</a>
-                            </li>
-                        </ul>
-                    @elseif (Auth::user()->role == 'Siswa')
-                        <ul class="list-group list-group-unbordered mb-3">
-                            <li class="list-group-item">
-                                <b>No INduk</b> <a class="float-right">{{ Auth::user()->no_induk }}</a>
-                            </li>
-                            <li class="list-group-item">
-                                <b>NIS</b> <a class="float-right">{{ Auth::user()->siswa(Auth::user()->no_induk)->nis }}</a>
-                            </li>
-                        </ul>
-                    @else
-                    @endif
-                    @if (Auth::user()->role !== 'admin')
+                    <h3 class="profile-username text-center">{{ $name }}</h3>
+                    <p class="text-muted text-center"> Role : {{ $user->role() }}</p>
+                    @if ($user->role !== 'admin')
                     <a href="{{ route('pengaturan.profile') }}" class="btn btn-primary btn-block"><b>Edit Profile</b></a>
                     @endif
                 </div>
@@ -76,40 +69,22 @@
                 </div>
                 <!-- /.card-header -->
                 <div class="card-body">
+                    @if ($user->role == 'guru')
+                        <strong><i class="far fa-envelope mr-1"></i> NIP</strong>
+                        <p class="text-muted">{{ $guru->nip }}</p>
+                        <hr>
+                    @endif
+
                     <strong><i class="far fa-envelope mr-1"></i> Email</strong>
-                    <p class="text-muted">{{ Auth::user()->email }}</p>
+                    <p class="text-muted">{{ $user->email }}</p>
                     <hr>
 
-                    @if (Auth::user()->role == 'guru')
-                        <strong><i class="fas fa-map-marker-alt mr-1"></i> Tempat Lahir</strong>
-                        <p class="text-muted">{{ Auth::user()->guru(Auth::user()->id_card)->tmp_lahir }}</p>
-                        <hr>
-                    @elseif (Auth::user()->role == 'Siswa')
-                        <strong><i class="fas fa-map-marker-alt mr-1"></i> Tempat Lahir</strong>
-                        <p class="text-muted">{{ Auth::user()->siswa(Auth::user()->no_induk)->tmp_lahir }}</p>
-                        <hr>
-                    @else
-                    @endif
+                    <strong><i class="far fa-calendar mr-1"></i> Tanggal Lahir</strong>
+                    <p class="text-muted">{{ date('l, d F Y', strtotime($guru->tgl_lahir)) }}</p>
+                    <hr>
 
-                    @if (Auth::user()->role == 'guru')
-                        <strong><i class="far fa-calendar mr-1"></i> Tanggal Lahir</strong>
-                        <p class="text-muted">{{ date('l, d F Y', strtotime(Auth::user()->guru(Auth::user()->id_card)->tgl_lahir)) }}</p>
-                        <hr>
-                    @elseif (Auth::user()->role == 'Siswa')
-                        <strong><i class="far fa-calendar mr-1"></i> Tanggal Lahir</strong>
-                        <p class="text-muted">{{ date('l, d F Y', strtotime(Auth::user()->siswa(Auth::user()->no_induk)->tgl_lahir)) }}</p>
-                        <hr>
-                    @else
-                    @endif
-
-                    @if (Auth::user()->role == 'guru')
-                        <strong><i class="fas fa-phone mr-1"></i> No Telepon</strong>
-                        <p class="text-muted">{{ Auth::user()->guru(Auth::user()->id_card)->telp }}</p>
-                    @elseif (Auth::user()->role == 'Siswa')
-                        <strong><i class="fas fa-phone mr-1"></i> No Telepon</strong>
-                        <p class="text-muted">{{ Auth::user()->siswa(Auth::user()->no_induk)->telp }}</p>
-                    @else
-                    @endif
+                    <strong><i class="fas fa-phone mr-1"></i> No Telepon</strong>
+                    <p class="text-muted">{{ $guru->telp }}</p>
                 </div>
                 <!-- /.card-body -->
             </div>
