b829e2ad69a0c78a079365f890f6d61c2cef8805
Syifa' Ul Haq M <syifaulhaq80@gmail.com>
2023-12-13 22:41:57 +0700

    update database terisi terbaru
    Signed-off-by: Syifa' Ul Haq M <syifaulhaq80@gmail.com>

--- monitoring.sql
+++ monitoring.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 13, 2023 at 12:28 PM
+-- Generation Time: Dec 13, 2023 at 04:41 PM
 -- Server version: 10.4.25-MariaDB
 -- PHP Version: 8.1.10
 
