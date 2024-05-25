1bcfd19f11ccafc40ece57c8d5c1a737f0bf0a17
Syifa' Ul Haq M <syifaulhaq80@gmail.com>
2024-05-25 15:48:06 +0700

    feat: notifikasi panel

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,150 +1,190 @@
 @extends('template.home')
-@section('heading', 'Dashboard '.Auth::user()->role())
+@section('heading', 'Dashboard ' . Auth::user()->role())
 @section('page')
-  <li class="breadcrumb-item active">Dashboard</li>
+    <li class="breadcrumb-item active">Dashboard</li>
 @endsection
 @section('content')
-<div class="col-md-12">
-    <div class="card card-warning" style="min-height: 385px;">
-        <div class="card-header">
-            <h3 class="card-title" style="color: white;">
-            Pengumuman
-            </h3>
-        </div>
-        <div class="card-body">
-            <div class="tab-content p-0">
-                <?php
-                $isi = trim($pengumuman->isi);
-                if ($isi === "") {
-                $isi = "<h3>Tidak ada pengumuman!</h3>";
-                }
-                ?>
-                {!! $isi !!}
+    <div class="col-md-12">
+        <div class="card card-warning" style="min-height: 385px;">
+            <div class="card-header">
+                <h3 class="card-title" style="color: white;">
+                    Pengumuman
+                </h3>
             </div>
-        </div>
-    </div>
-</div>  
-<div class="col-lg-6">
-    <div class="card">
-        <div class="card-body">
-            <div class="d-flex">
-                <p class="d-flex flex-column">
-                    <span class="text-bold text-lg">Data Guru</span>
-                </p>
-                <p class="ml-auto d-flex flex-column text-right">
-                    <span class="text-success">
-                        <h2> Total Guru {{ $guru_all }} </h2>
-                    </span>
-                </p>
-            </div>
-            <div class="position-relative mb-4">
-                <div class="row">
-                    <div class="col-md-8">
-                        <div class="chart-responsive">
-                            <canvas id="pieChartGuru" height="200"></canvas>
-                        </div>
-                    </div>
-                    <div class="col-md-4">
-                        <ul class="chart-legend clearfix">
-                            <li><i class="far fa-circle text-primary"></i> Laki-laki</li>
-                            <li><i class="far fa-circle text-danger"></i> Perempuan</li>
-                        </ul>
-                    </div>
+            <div class="card-body">
+                <div class="tab-content p-0">
+                    <?php
+                    $isi = trim($pengumuman->isi);
+                    if ($isi === '') {
+                        $isi = '<h3>Tidak ada pengumuman!</h3>';
+                    }
+                    ?>
+                    {!! $isi !!}
                 </div>
             </div>
         </div>
     </div>
-</div>
-<div class="col-lg-6">
-    <div class="card">
-        <div class="card-body">
-            <div class="d-flex">
-                <p class="d-flex flex-column">
-                    <span class="text-bold text-lg">Data Siswa</span>
-                </p>
-                <p class="ml-auto d-flex flex-column text-right">
-                    <span class="text-success">
-                        <h2> Total Siswa {{ $siswa_all }} </h2>
-                    </span>
-                </p>
+    <div class="col-md-6">
+        <div class="card card-info" style="min-height: 385px;">
+            <div class="card-header">
+                <h3 class="card-title" style="color: white;">
+                    Pemberitahuan
+                </h3>
             </div>
-            <div class="position-relative mb-4">
-                <div class="row">
-                    <div class="col-md-8">
-                        <div class="chart-responsive">
-                            <canvas id="pieChartSiswa" height="200"></canvas>
-                        </div>
+            <div class="card-body">
+                @if (Auth::user()->role === 'guru')
+                    <div class="alert alert-info" role="alert">
+                        Notifikasi untuk guru
                     </div>
-                    <div class="col-md-4">
-                        <ul class="chart-legend clearfix">
-                            <li><i class="far fa-circle text-primary"></i> Laki-laki</li>
-                            <li><i class="far fa-circle text-danger"></i> Perempuan</li>
-                        </ul>
+                @elseif(Auth::user()->role === 'orang_tua')
+                    <div class="alert alert-info" role="alert">
+                        Notifikasi untuk orang tua
+                    </div>
+                @else
+                    <div class="alert alert-danger" role="alert">
+                        Anda tidak memiliki akses untuk fitur ini
+                    </div>
+                @endif
+            </div>
+        </div>
+    </div>
+    <div class="col-md-6">
+        <div class="card card-success" style="min-height: 385px;">
+            <div class="card-header">
+                <h3 class="card-title" style="color: white;">
+                    Pencapaian
+                </h3>
+            </div>
+            <div class="card-body">
+                @if (Auth::user()->role === 'guru')
+                    <div class="alert alert-info" role="alert">
+                        Notifikasi untuk guru
+                    </div>
+                @elseif(Auth::user()->role === 'orang_tua')
+                    <div class="alert alert-info" role="alert">
+                        Notifikasi untuk orang tua
+                    </div>
+                @else
+                    <div class="alert alert-danger" role="alert">
+                        Anda tidak memiliki akses untuk fitur ini
+                    </div>
+                @endif
+                </>
+            </div>
+        </div>
+    </div>
+
+    @if (Auth::user()->role !== 'orang_tua')
+        <div class="col-lg-6">
+            <div class="card">
+                <div class="card-body">
+                    <div class="d-flex">
+                        <p class="d-flex flex-column">
+                            <span class="text-bold text-lg">Data Guru</span>
+                        </p>
+                        <p class="ml-auto d-flex flex-column text-right">
+                            <span class="text-success">
+                                <h2> Total Guru {{ $guru_all }} </h2>
+                            </span>
+                        </p>
+                    </div>
+                    <div class="position-relative mb-4">
+                        <div class="row">
+                            <div class="col-md-8">
+                                <div class="chart-responsive">
+                                    <canvas id="pieChartGuru" height="200"></canvas>
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <ul class="chart-legend clearfix">
+                                    <li><i class="far fa-circle text-primary"></i> Laki-laki</li>
+                                    <li><i class="far fa-circle text-danger"></i> Perempuan</li>
+                                </ul>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-</div>
+        <div class="col-lg-6">
+            <div class="card">
+                <div class="card-body">
+                    <div class="d-flex">
+                        <p class="d-flex flex-column">
+                            <span class="text-bold text-lg">Data Siswa</span>
+                        </p>
+                        <p class="ml-auto d-flex flex-column text-right">
+                            <span class="text-success">
+                                <h2> Total Siswa {{ $siswa_all }} </h2>
+                            </span>
+                        </p>
+                    </div>
+                    <div class="position-relative mb-4">
+                        <div class="row">
+                            <div class="col-md-8">
+                                <div class="chart-responsive">
+                                    <canvas id="pieChartSiswa" height="200"></canvas>
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <ul class="chart-legend clearfix">
+                                    <li><i class="far fa-circle text-primary"></i> Laki-laki</li>
+                                    <li><i class="far fa-circle text-danger"></i> Perempuan</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    @endif
 
 @endsection
 @section('script')
     <script>
-      $("#Dashboard").addClass("active");
-      $("#liDashboard").addClass("menu-open");
-      $("#Home").addClass("active");
-
-      $(document).ready(function () {
-            'use strict'
-
-            var pieChartCanvasGuru = $('#pieChartGuru').get(0).getContext('2d')
-            var pieDataGuru        = {
+        $(document).ready(function() {
+            var pieChartCanvasGuru = $('#pieChartGuru').get(0).getContext('2d');
+            var pieDataGuru = {
                 labels: [
-                    'Laki-laki', 
-                    'Perempuan',
+                    'Laki-laki', 'Perempuan',
                 ],
-                datasets: [
-                    {
+                datasets: [{
                     data: [{{ $guru_lk }}, {{ $guru_pr }}],
-                    backgroundColor : ['#007BFF', '#DC3545'],
-                    }
-                ]
-            }
-            var pieOptions     = {
+                    backgroundColor: ['#007BFF', '#DC3545'],
+                }]
+            };
+            var pieOptionsGuru = {
                 legend: {
                     display: false
                 }
-            }
-            var pieChart = new Chart(pieChartCanvasGuru, {
+            };
+            var pieChartGuru = new Chart(pieChartCanvasGuru, {
                 type: 'doughnut',
                 data: pieDataGuru,
-                options: pieOptions      
-            })
+                options: pieOptionsGuru
+            });
 
 
-            var pieChartCanvasSiswa = $('#pieChartSiswa').get(0).getContext('2d')
-            var pieDataSiswa        = {
+            var pieChartCanvasSiswa = $('#pieChartSiswa').get(0).getContext('2d');
+            var pieDataSiswa = {
                 labels: [
-                    'Laki-laki', 
-                    'Perempuan',
+                    'Laki-laki', 'Perempuan',
                 ],
-                datasets: [
-                    {
+                datasets: [{
                     data: [{{ $siswa_lk }}, {{ $siswa_pr }}],
-                    backgroundColor : ['#007BFF', '#DC3545'],
-                    }
-                ]
-            }
-            var pieOptions     = {
+                    backgroundColor: ['#007BFF', '#DC3545'],
+                }]
+            };
+            var pieOptionsSiswa = {
                 legend: {
                     display: false
                 }
-            }
-            var pieChart = new Chart(pieChartCanvasSiswa, {
+            };
+            var pieChartSiswa = new Chart(pieChartCanvasSiswa, {
                 type: 'doughnut',
                 data: pieDataSiswa,
-                options: pieOptions      
-            })
-      });
+                options: pieOptionsSiswa
+            });
+        });
     </script>
 @endsection
